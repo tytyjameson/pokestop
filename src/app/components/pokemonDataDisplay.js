a576342e0d7a79af22bef995550500b3151a95c3
Tyler Martinez <tm6002005@gmail.com>
2025-10-18 23:30:46 -0700

    update PokemonDataDisplay with basic info for state

--- src/app/components/pokemonDataDisplay.js
+++ src/app/components/pokemonDataDisplay.js
@@ -2,6 +2,19 @@ import React,{useState} from 'react';
 
 const pokemonDataDisplay = () =>{
 
+   const [ displayName, setDisplayName ] = useState("");
+   const [ cardSet, setCardSet ] = useState("");
+   const [ evolutionLevel, setEveolutionLevel ] =  useState("");
+   const [ types, setTypes ] = useState("");
+   const [ image, setImage ] = useState("");
+   const [ tcgplayerMidPrice, setTcgplayerMidPrice ] = useState("");
+
+    return(
+        <div id="dispay-container">
+          <h3 id="name">{pokemonName}</h3>
+
+        </div>
+    )
 }
 
 export default pokemonDataDisplay;  
