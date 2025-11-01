b0f6a72fdaa969e9988a74845449b1cd4d0f2f16
Tyler Martinez <tm6002005@gmail.com>
2025-10-31 23:15:26 -0700

    holding fetch in the frontend for now, going to move to backend later

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,28 @@ import Image from "next/image";
 import PokemonDataDisplay from './components/pokemonDataDisplay';
 import styles from "./page.module.css";
 
-export default function Home() {
-  const API_KEY='';//API from card site and tcg
-  const [name, setName] = useState("");
+//hold this here for now, have to make API call from the backend 
+export const getStaticProps = async () => {
+  const API_KEY='90ec5b1d-ba3f-4743-9487-4259b89eb628';//API from card site and tcg
+    const response = await fetch(`https://api.pokemontcg.io/v2/cards`, {
+      method: 'GET',
+      headers:{
+        'X-Api-Key': API_KEY,
+        },
+      })
+    const data = await response.JSON();
+    console.log(data)
+  return {
+    props:{pokemonData: data}
+    }
+  }
 
+  console.log('this is the big pokemon object', pokemonData)
+
+function Home({pokemonData}) {
+  // const API_KEY='';//API from card site and tcg
+  const [name, setName] = useState("");
+  console.log('this is the big pokemon object', pokemonData)
   const handlePokemonInput = (event) => {
     setName(event.target.value);
   
@@ -19,16 +37,16 @@ export default function Home() {
   const submitPokemon = (event) => {
     event.preventDefault();
     console.log("this is the pokemon you're looking for", name);
-    fetch(`https://api.pokemontcg.io/v2/cards`, {
-      method: 'GET',
-      mode: 'cors',
-      headers:{
-        'X-Api-Key': API_KEY,
-        },
-      })
-      .then((response) => response.json())
-      .then((data) => console.log('this is the data retreived', data))
-      .catch((err) => console.log('ERROR:', err))
+    // fetch(`https://api.pokemontcg.io/v2/cards`, {
+    //   method: 'GET',
+    //   mode: 'cors',
+    //   headers:{
+    //     'X-Api-Key': API_KEY,
+    //     },
+    //   })
+    //   .then((response) => response.json())
+    //   .then((data) => console.log('this is the data retreived', data))
+    //   .catch((err) => console.log('ERROR:', err))
       // console.log('this is the pokemon call via name query',pokemon)
   }
   
@@ -55,3 +73,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home;
