56274ba9a0f7bc3b12b74147903e1d3e7cbcab05
Tyler Martinez <tm6002005@gmail.com>
2025-11-06 20:07:57 -0800

    finished rough display data

--- src/app/components/pokemonDataDisplay.js
+++ src/app/components/pokemonDataDisplay.js
@@ -11,9 +11,16 @@ const pokemonDataDisplay = (props) =>{
 
     return(
         <div id="dispay-container">
-          <h3 id="name">{displayName}</h3>
-          <h5 id="cardSet">{cardSet}</h5>
-
+          <div id="mainData">
+            <h3 id="name">{displayName}</h3>
+            <img id="image">{image}</img>
+            <h5 id="cardSet">{cardSet}</h5>
+            <h6 id="midPrice">{tcgplayerMidPrice}</h6>
+          </div>
+          <div id="lesserData">
+            <span id="evolutionLevel">{evolutionLevel}</span>
+            <span id="types">{types}</span>
+          </div>
         </div>
     )
 }
